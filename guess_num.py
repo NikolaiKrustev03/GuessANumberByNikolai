2273a06ff2f0b484f850a82d1ad89328261c7c68
NikolaiKrustev03 <n.krastev2003@gmail.com>
2023-04-11 17:04:48 +0300

    Update guess_num.py

--- guess_num.py
+++ guess_num.py
@@ -1,15 +1,35 @@
 import random
-to_be_guessed=random.randint(1,100)
-passing=False
+
+def guess_num():
+    to_be_guessed = random.randint(1, 100)
+    print(to_be_guessed)
+    while True:
+        try:
+            player_input = int(input("Guess my number (1-100): "))
+            if player_input == to_be_guessed:
+                print("Congratulations, you guessed!")
+                break
+            elif 1<=player_input < to_be_guessed:
+                print("Higher")
+            elif 100>=player_input > to_be_guessed:
+                print("Lower")
+            else:
+                print(f"{player_input} isn't a number between 1 and 100")
+        except ValueError:
+            print("Only integers allowed")
+
+guess_num()
 while True:
     try:
-        player_input=int(input("Guess my number (1-100): "))
-        if player_input==to_be_guessed:
-            print("Congratulations, you guessed!")
+        continue_game = input("Would you like to continue? (Y/N)")
+        if continue_game=="Y":
+            guess_num()
+        elif continue_game=="N":
+            print("Maybe you'd like to check out my other projects on https://github.com/NikolaiKrustev03")
             break
-        elif player_input<to_be_guessed:
-            print("Higher")
-        elif player_input>to_be_guessed:
-            print("Lower")
+        elif not continue_game.isalpha():
+            print(f"{continue_game} isn't even an alphabetic character")
+        else:
+            print("Only Y or N as a valid answer")
     except ValueError:
-        print("Only integers allowed")
+        print("We got a value error")
